2f09bc9278c86b5d59f1fd3e2257bd72fb2b5fbf
superGemHere <122823771+superGemHere@users.noreply.github.com>
2024-02-21 19:30:31 +0200

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,6 @@
 import { useState } from 'react'
 import {Routes, Route} from 'react-router-dom'
 
-import './App.css'
-
 import Home from './components/main/Home/Home.jsx';
 import Product from './components/main/Product/Product.jsx';
 import Products from './components/main/Products/Products.jsx';
@@ -10,10 +8,12 @@ import Products from './components/main/Products/Products.jsx';
 import Navbar from './components/outlet/Navbar/Navbar.jsx';
 import Footer from './components/outlet/Footer/Footer.jsx';
 
+import './app.scss'
+
 function App() {
 
   return (
-    <>
+    <div  className= "app">
     <Navbar />
 
       <Routes>
@@ -23,9 +23,9 @@ function App() {
             <Route path="/products/:id" element={<Products />} />
 
       </Routes>
-      
+
     <Footer />
-    </>
+    </div>
   )
 }
 
