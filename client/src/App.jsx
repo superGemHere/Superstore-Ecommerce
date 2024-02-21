b2270336a651d0c1ad0dbf136c3a2a226e5bb8f8
superGemHere <122823771+superGemHere@users.noreply.github.com>
2024-02-21 17:59:56 +0200

    +Header and Footer components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,14 +3,19 @@ import {Routes, Route} from 'react-router-dom'
 
 import './App.css'
 
-import Home from './components/main/Home/Home.jsx'
-import Product from './components/main/Product/Product.jsx'
-import Products from './components/main/Products/Products.jsx'
+import Home from './components/main/Home/Home.jsx';
+import Product from './components/main/Product/Product.jsx';
+import Products from './components/main/Products/Products.jsx';
+
+import Navbar from './components/outlet/Navbar/Navbar.jsx';
+import Footer from './components/outlet/Footer/Footer.jsx';
 
 function App() {
 
   return (
     <>
+    <Navbar />
+
       <Routes>
 
             <Route path="/" element={<Home />} />
@@ -18,7 +23,8 @@ function App() {
             <Route path="/products/:id" element={<Products />} />
 
       </Routes>
-
+      
+    <Footer />
     </>
   )
 }
