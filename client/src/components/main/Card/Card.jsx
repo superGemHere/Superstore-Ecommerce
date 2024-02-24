be5a7daeff7981e1b1087f1d4d582974d9169fbf
superGemHere <122823771+superGemHere@users.noreply.github.com>
2024-02-24 20:46:14 +0200

    create card layout

--- client/src/components/main/Card/Card.jsx
+++ client/src/components/main/Card/Card.jsx
@@ -1,10 +1,24 @@
 
+import {Link} from 'react-router-dom'
+
 import "./Card.scss"
 
-export default function Card(){
+export default function Card({item}){
 
     return(
-        <div className="card">CARD</div>
+        <Link to={`/product/${item._id}`}>
+        <div className="card">
+            <div className="image">
+                <img src={item.img1} alt={`${item.title} image`} className="mainImg" />
+                <img src={item.img2} alt={`${item.title} image`} className="secondaryImg" />
+            </div>
+            <h2>{item.title}</h2>
+            <div className="prices">
+               <h3>${item.oldPrice}</h3> 
+               <h3>${item.price}</h3> 
+            </div>
+        </div>
+        </Link>
     );
 
 }
