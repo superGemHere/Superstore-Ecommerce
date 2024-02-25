dfc6e5af04cccd89bb049299ccde414025a29af3
superGemHere <122823771+superGemHere@users.noreply.github.com>
2024-02-25 18:38:45 +0200

    ADD Categories

--- client/src/components/main/Home/Home.jsx
+++ client/src/components/main/Home/Home.jsx
@@ -1,5 +1,6 @@
 import FeaturedProducts from "../FeaturedProducts/FeaturedProducts";
 import Slider from "../Slider/Slider";
+import Categories from "../Categories/Categories";
 
 import "./home.scss"
 
@@ -9,6 +10,7 @@ export default function Home(){
         <div className="home">
             <Slider />
             <FeaturedProducts type="featured" />
+            <Categories />
             <FeaturedProducts type="trending" />
         </div>
     );
