564444807c7eb6d956295f2961489f9383dc09b1
superGemHere <122823771+superGemHere@users.noreply.github.com>
2024-02-26 17:13:37 +0200

    add contact component

--- client/src/components/main/Home/Home.jsx
+++ client/src/components/main/Home/Home.jsx
@@ -1,6 +1,7 @@
 import FeaturedProducts from "../FeaturedProducts/FeaturedProducts";
 import Slider from "../Slider/Slider";
 import Categories from "../Categories/Categories";
+import Contact from "../Contact/Contact";
 
 import "./home.scss"
 
@@ -12,6 +13,7 @@ export default function Home(){
             <FeaturedProducts type="featured" />
             <Categories />
             <FeaturedProducts type="trending" />
+            <Contact />
         </div>
     );
 }
