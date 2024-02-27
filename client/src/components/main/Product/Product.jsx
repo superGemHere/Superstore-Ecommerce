d458f2133f0da9af62876e40443f1a5c7ca457b5
superGemHere <122823771+superGemHere@users.noreply.github.com>
2024-02-27 19:27:01 +0200

    Update Product.jsx

--- client/src/components/main/Product/Product.jsx
+++ client/src/components/main/Product/Product.jsx
@@ -3,8 +3,6 @@ import "./product.scss"
 
 export default function Product(){
 
-    const arrowDown = String.fromCodePoint(8595);
-    const arrowUp = String.fromCodePoint(8593);
 
     return(
         <div className="product">
