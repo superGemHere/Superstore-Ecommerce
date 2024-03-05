08fdcb29cff23977eca4c800bce754ef1810d9d0
superGemHere <122823771+superGemHere@users.noreply.github.com>
2024-03-05 18:09:40 +0200

    Update store.js

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,3 +1,6 @@
+// const stripe = require('stripe')('sk_test_51OquvSHVlf1zZe1LZwDOHArIP5GcPzj5TtV77K1xgVwDOwsEJu1Jd8yiu0TnaMHc7XGDiCNAHI90QWEZsyTkWbbB00lFjtwGpD');
+
+
 import cartReducer from './cartReducer'
 
 import { configureStore } from '@reduxjs/toolkit'
@@ -11,7 +14,9 @@ import {
   PURGE,
   REGISTER,
 } from 'redux-persist'
-import storage from 'redux-persist/lib/storage'
+
+import storage from 'redux-persist/lib/storage';
+
 
 
 
